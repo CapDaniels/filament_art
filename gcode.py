major bugfix in the distance calculation and first working prototype state

gcode.py
@@ -7,7 +7,7 @@ import contours
 
 
 def L2_norm(v1: npt.NDArray, v2: npt.NDArray):
-    return np.sqrt(np.mean(np.square(v2 - v1), axis=-1))
+    return np.sqrt(np.sum(np.square(v2 - v1), axis=-1))
 
 
 class GSketch(MutableSequence):
@@ -132,6 +132,7 @@ class G1(GCode):
 
 
 class GThinStretch(G1):
+    # could be also implemented as a classmethod, but like the separation
     def __init__(self, thickness, x=None, y=None, z=None, f=None):
         gsketch = GSketch._current_gsketch
 
@@ -156,19 +157,19 @@ class GThinStretch(G1):
         # distance times the fraction of target area and filament cross section area  (shortend some terms)
         e = dist * (thickness / gsketch.filament_diameter) ** 2
 
-        super().__init__(x=x_new, y=y_new, z=z_new, e=e)
+        super().__init__(x=x_new, y=y_new, z=z_new, e=e, f=f)
 
 
 def GString(
-    thickness: float, x: float | None = None, y: float | None = None, z_hop=0.4, extrude_at_lift=True
+    thickness: float, x: float | None = None, y: float | None = None, z_hop=0.4, extrude_at_lift=True, vf=600., hf=1000.
 ):
     z_base = GSketch._current_gsketch.get_curr_pos(["Z"])[0]
     if extrude_at_lift:
-       GThinStretch(thickness, z=z_base + z_hop)
+       GThinStretch(thickness, z=z_base + z_hop, f=vf)
     else:
-        G1(z=z_base + z_hop)
-    GThinStretch(thickness, x=x, y=y)
-    G1(z=z_base)
+        G1(z=z_base + z_hop, f=vf)
+    GThinStretch(thickness, x=x, y=y, f=hf)
+    G1(z=z_base, f=vf)
 
 
 def GFollowContour(contour: contours.Contour, s1: float, s2: float, stepsize=0.0025):
@@ -197,9 +198,9 @@ if __name__ == "__main__":
     GThinStretch(0.2, 5, 5, 5)
     gcode_str = gsketch[-1].getGstring()
 
-    target_test_gcode = "G1 X5.000 Y5.000 Z5.000 E0.03845"
-    assert gcode_str == target_test_gcode
+    target_test_gcode = "G1 X5.000 Y5.000 Z5.000 E0.06660"
     print(gsketch.get_GCode())
+    assert gcode_str == target_test_gcode
 
     circle = contours.Circle((0, 0), 10)
     GFollowContour(circle, 0.5, 0.25)
@@ -207,3 +208,5 @@ if __name__ == "__main__":
     # print(GFollowContour(circle, 0.25, 0.99))
     # print(GFollowContour(circle, 0.99, 0.25))
     print(gsketch.get_GCode())
+
+    print(L2_norm(np.array([[1,1,0], [10,0,0]]), np.zeros((2,3))))

solver_discrete_no_hop.py
@@ -131,7 +131,7 @@ class Solver:
             opacity  # only used in drawing, but not in finding the best move
         )
         self.overlap_handling = "kink"  # internal solver setting without user accsess
-        self.kink_factor = 0.1  # internal solver setting without user accsess
+        self.kink_factor = 0.25  # internal solver setting without user accsess
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         self.img = np.asarray(img, dtype=np.float32) / 255
         contour_dx, contour_dy = self.contour.get_extension()  # in mm

test1.py
@@ -7,38 +7,42 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    circle = contours.Circle((0, 0), 100)
+    circle = contours.Circle((110, 110), 100)
+    pic_name="Snoopy_Peanuts.png"
     img = cv2.imread(
-        r".\..\string_art\test_images\Snoopy_Peanuts.png"
+        fr".\..\string_art\test_images\{pic_name}"
     )
     img_weights = None
     # img_weights = cv2.imread(
-    #     r".\..\string_art\test_images\11_mask.jpg"
+    #     fr".\..\string_art\test_images\11_mask.jpg"
     # )
 
-
-    # solver = SolverGUI(circle, img, img_weights=img_weights, line_thickness=0.2, dpmm=5.0, n_points=500)
+    thickness= 0.4
+    solver = SolverGUI(circle, img, img_weights=img_weights, line_thickness=thickness, dpmm=5.0, n_points=500)
     # solver.start_gui()
-    # gsketch = GSketch("test_main", nozzle_diameter=0.4, filament_diameter=1.75)
-    # gcode_stringer = GcodeStringer(solver, gsketch)
-    # gcode_stringer.process_Gcode()
-    # # print(gsketch.get_GCode())
-    # gsketch.save_GCode("./test/test1.gcode")
-    # cv2.imwrite( "./test/test1.png", solver.image)
+    for i in range(500):
+        solver.solve_next()
+    gsketch = GSketch("test_main", nozzle_diameter=0.4, filament_diameter=1.75)
+    gcode_stringer = GcodeStringer(solver, gsketch, z_hop=1, z_base=5, feed_rate=1400., thickness=thickness)
+    gcode_stringer.process_Gcode()
+    # print(gsketch.get_GCode())
+    gsketch.save_GCode(f"./test/{pic_name}.gcode")
+    cv2.imwrite(f"./test/{pic_name}", solver.image)
+
 
 
 
     # test headleass
-    kf = 0.3
-    for kf in np.arange(0, 1, 0.025):
-        print("KF=", kf)
-        solver = Solver(circle, img, img_weights=img_weights, line_thickness=0.2, dpmm=5.0, n_points=500)
-        solver.kink_factor = kf
-        for i in range(600):
-            solver.solve_next()
-        gsketch = GSketch("test_main", nozzle_diameter=0.4, filament_diameter=1.75)
-        gcode_stringer = GcodeStringer(solver, gsketch)
-        gcode_stringer.process_Gcode()
-        # print(gsketch.get_GCode())
-        # gsketch.save_GCode("./test/test1.gcode")
-        cv2.imwrite(f"./test/test1_headless_kf{kf:.2f}.png", solver.image)
+    # kf = 0.3
+    # for kf in np.arange(0, 0.35, 0.05):
+    #     print("KF=", kf)
+    #     solver = Solver(circle, img, img_weights=img_weights, line_thickness=0.2, dpmm=5.0, n_points=500)
+    #     solver.kink_factor = kf
+    #     for i in range(1100):
+    #         solver.solve_next()
+    #     gsketch = GSketch("test_main", nozzle_diameter=0.4, filament_diameter=1.75)
+    #     gcode_stringer = GcodeStringer(solver, gsketch)
+    #     gcode_stringer.process_Gcode()
+    #     # print(gsketch.get_GCode())
+    #     # gsketch.save_GCode("./test/test1.gcode")
+    #     cv2.imwrite(f"./test/{pic_name}_headless_kf{kf:.3f}.png", solver.image)