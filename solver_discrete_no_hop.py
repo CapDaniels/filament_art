5ff798d70bc14f0976b13beb0dac6e2d5a494dc7
Daniel Rech <daniel.rech@student.kit.edu>
2024-08-05 00:06:42 +0200

    changed GUI to mainloop based

--- solver_discrete_no_hop.py
+++ solver_discrete_no_hop.py
@@ -4,6 +4,7 @@ import numpy as np
 import numpy.typing as npt
 from contours import Contour, Circle
 import skimage.transform as transform
+
 # from iminuit import Minuit
 from line_alg import wu_line
 from numba import njit
@@ -17,13 +18,16 @@ from gcode import GSketch
 from gcode_builder import GcodeStringer
 import warnings
 from pathlib import Path
+import time
 
 
 img_type = cv2.typing.MatLike
 
 
 @njit
-def darkness_along_line_aa_njit(target, x1, y1, x2, y2, thickness, img_weights=None):
+def darkness_along_line_aa_njit(
+    target, x1, y1, x2, y2, thickness, img_weights=None
+):
     """
     Calculate the average darkness along a line in the target image.
 
@@ -139,7 +143,9 @@ class Solver:
         self.opacitys = (
             opacitys  # only used in the solver target, but not for drawing
         )
-        self.overlap_handling = "kink"  # internal solver setting without user accsess
+        self.overlap_handling = (
+            "kink"  # internal solver setting without user accsess
+        )
         self.kink_factor = kink_factor
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         self.img = np.asarray(img, dtype=np.float32) / 255
@@ -158,14 +164,16 @@ class Solver:
                 round(img_width * self.img_scale_factor + 1),
             ),
             anti_aliasing=True,
-        ) # type: ignore
+        )  # type: ignore
         self._img = np.asarray(
             img, dtype=np.float32
         )  # internal working copy that is rescaled and will be modified
         # line thickness in dots for internal use
         self._line_thickness = line_thickness * dpmm
         if self._line_thickness < 1.0:
-            warnings.warn(f"Your DPI is low for the selected thickness. A value above {1/line_thickness:.2f} is adviced.")
+            warnings.warn(
+                f"Your DPI is low for the selected thickness. A value above {1/line_thickness:.2f} is adviced."
+            )
         self._border_padding = int(np.ceil(self._line_thickness / 2)) + 1
         self._img = cv2.copyMakeBorder(
             self._img,
@@ -180,7 +188,9 @@ class Solver:
             self.img_weights = np.asarray(img_weights, dtype=np.float32) / 255
             imgw_height, imgw_width = img_weights.shape
             if (img_height != imgw_height) or (img_width != imgw_width):
-                raise ValueError("Image and weight image must have the same size!")
+                raise ValueError(
+                    "Image and weight image must have the same size!"
+                )
             img_weights = transform.resize(
                 img_weights,
                 (
@@ -188,8 +198,10 @@ class Solver:
                     round(img_width * self.img_scale_factor + 1),
                 ),
                 anti_aliasing=True,
-            ) # type: ignore
-            img_weights = weights_importance * img_weights + ( 1- weights_importance)
+            )  # type: ignore
+            img_weights = weights_importance * img_weights + (
+                1 - weights_importance
+            )
             self._img_weights = np.asarray(
                 img_weights, dtype=np.float32
             )  # internal working copy that is rescaled and will be modified
@@ -207,14 +219,21 @@ class Solver:
         self._img_line_count = self._img.copy()
         self.string_count = 0
         self.n_points = n_points
-        self.s_vals = np.linspace(0.0, 1.0, n_points+1, endpoint=False)
-        self.xy_points = np.array([self._get_coordinates(s) for s in self.s_vals])
+        self.s_vals = np.linspace(0.0, 1.0, n_points + 1, endpoint=False)
+        self.xy_points = np.array(
+            [self._get_coordinates(s) for s in self.s_vals]
+        )
         self.curr_sidx = None
         self._solved_first = False
         self.s_connections = []
-        self.sidx_connections = np.full([n_points, n_points], False, dtype=bool)  # adjacency matrix to avoid doubling the strings
-        self._img_canvas = np.ones(self._img.shape)  # to draw the resulting image
+        self.sidx_connections = np.full(
+            [n_points, n_points], False, dtype=bool
+        )  # adjacency matrix to avoid doubling the strings
+        self._img_canvas = np.ones(
+            self._img.shape
+        )  # to draw the resulting image
 
+        self.curr_score = 0
 
     def _get_coordinates(self, s1):
         # getting the shifted corrdinates, so that the min is [0, 0]
@@ -238,7 +257,7 @@ class Solver:
             diff = 1 - diff
         if diff > 0.1:
             return 0.0
-        return ((0.1 - diff) * 10)**2
+        return ((0.1 - diff) * 10) ** 2
 
     def _build_target_function(self):
         """
@@ -260,13 +279,18 @@ class Solver:
         """
         match self.mode:
             case "greedy_dark":
+
                 def minimization_target(xy1, xy2):
                     # penalty term, if the distance is too small
                     # squared increase from 0 to 1 for values of abs(s2-s1)<0.1
-                    error = (
-                        darkness_along_line_aa_njit(
-                            self._img, xy1[0], xy1[1], xy2[0], xy2[1], self._line_thickness, self._img_weights
-                        )
+                    error = darkness_along_line_aa_njit(
+                        self._img,
+                        xy1[0],
+                        xy1[1],
+                        xy2[0],
+                        xy2[1],
+                        self._line_thickness,
+                        self._img_weights,
                     )
                     return error
 
@@ -293,8 +317,20 @@ class Solver:
         best_sidx = None
         f = self._build_target_function()
         # only do connections which do not already exist
-        possible_connections = np.arange(self.n_points, dtype=int)[~np.logical_or(self.sidx_connections[self.curr_sidx, :], self.sidx_connections[:, self.curr_sidx])]
-        scores = [f(self.xy_points[self.curr_sidx], self.xy_points[idx]) + self._closeness_penalty(self.s_vals[self.curr_sidx], self.s_vals[idx]) for idx in possible_connections if not idx == self.curr_sidx]
+        possible_connections = np.arange(self.n_points, dtype=int)[
+            ~np.logical_or(
+                self.sidx_connections[self.curr_sidx, :],
+                self.sidx_connections[:, self.curr_sidx],
+            )
+        ]
+        scores = [
+            f(self.xy_points[self.curr_sidx], self.xy_points[idx])
+            + self._closeness_penalty(
+                self.s_vals[self.curr_sidx], self.s_vals[idx]
+            )
+            for idx in possible_connections
+            if not idx == self.curr_sidx
+        ]
 
         best_score_idx = np.argmin(scores)
         best_fval = scores[best_score_idx]
@@ -315,6 +351,7 @@ class Solver:
             self.sidx_connections[old_sidx, self.curr_sidx] = True
         else:
             self.sidx_connections[self.curr_sidx, old_sidx] = True
+        self.curr_score = best_fval
         return old_sidx, best_sidx
 
     def _solve_first(self):
@@ -324,8 +361,14 @@ class Solver:
         best_sidxs = [0, 0]
         best_conn_idx = 0
         f = self._build_target_function()
-        possible_connections = list(itertools.combinations(range(self.n_points), 2))
-        scores = [f(self.xy_points[idx1], self.xy_points[idx2]) + self._closeness_penalty(self.s_vals[idx1], self.s_vals[idx2]) for idx1, idx2 in possible_connections]
+        possible_connections = list(
+            itertools.combinations(range(self.n_points), 2)
+        )
+        scores = [
+            f(self.xy_points[idx1], self.xy_points[idx2])
+            + self._closeness_penalty(self.s_vals[idx1], self.s_vals[idx2])
+            for idx1, idx2 in possible_connections
+        ]
 
         best_conn_idx = np.argmin(scores)
         best_fval = scores[best_conn_idx]
@@ -342,17 +385,22 @@ class Solver:
         self.s_connections.append(self.s_vals[best_sidxs[0]])
         self.s_connections.append(self.s_vals[best_sidxs[1]])
         if best_sidxs[0] > best_sidxs[1]:
-            best_sidxs[0], best_sidxs[1] = best_sidxs[1],  best_sidxs[0]
+            best_sidxs[0], best_sidxs[1] = best_sidxs[1], best_sidxs[0]
         self.sidx_connections[*best_sidxs,] = True
 
-        self.update_img_canvas(*best_sidxs,)
-        return *best_sidxs,
+        self.update_img_canvas(
+            *best_sidxs,
+        )
+        self.curr_score = best_fval
+        return (*best_sidxs,)
 
     def _update_img(self, sidx1, sidx2, opacity=None):
         if opacity is None:
             opacity = 1.0
         xy1, xy2 = self.xy_points[sidx1], self.xy_points[sidx2]
-        x, y, val = wu_line(xy1[0], xy1[1], xy2[0], xy2[1], thickness=self._line_thickness)
+        x, y, val = wu_line(
+            xy1[0], xy1[1], xy2[0], xy2[1], thickness=self._line_thickness
+        )
         old_px_vals = self._img[x, y]
         add_px_vals = np.array(val) * opacity
         match self.overlap_handling:
@@ -363,8 +411,14 @@ class Solver:
                 new_px_vals = old_px_vals + add_px_vals
                 self._img[x, y] = new_px_vals
             case "kink":
+
                 def func(x):
-                    return np.where(x < 1.0, x , (1 - self.kink_factor) + x * self.kink_factor)
+                    return np.where(
+                        x < 1.0,
+                        x,
+                        (1 - self.kink_factor) + x * self.kink_factor,
+                    )
+
                 self._img_line_count[x, y] += add_px_vals
                 self._img[x, y] = func(self._img_line_count[x, y])
 
@@ -372,25 +426,27 @@ class Solver:
         if opacity is None:
             opacity = self.opacityd
         xy1, xy2 = self.xy_points[sidx1], self.xy_points[sidx2]
-        x, y, val = wu_line(xy1[0], xy1[1], xy2[0], xy2[1], thickness=self._line_thickness)
+        x, y, val = wu_line(
+            xy1[0], xy1[1], xy2[0], xy2[1], thickness=self._line_thickness
+        )
         new_px_vals = img[x, y] + np.array(val) * opacity
         if do_clip:
             new_px_vals = np.clip(new_px_vals, 0.0, 1.0)
         img[x, y] = new_px_vals
         return img
-    
+
     def update_img_canvas(self, s1, s2):
         self._img_canvas = self.draw_line(
             s1, s2, self._img_canvas, -self.opacityd
         )
-    
+
     @property
     def image(self):
-        return np.clip(np.rint(self._img_canvas*255).astype(int), 0, 255)
-    
+        return np.clip(np.rint(self._img_canvas * 255).astype(int), 0, 255)
+
     def save_img(self, filename):
         cv2.imwrite(filename, self.image)
-    
+
     def save_project(self, path=None, n_segments=500, save_stl=True):
         if path is None:
             path = f"./{self.name}/"
@@ -405,7 +461,7 @@ class Solver:
 class SolverGUI(Solver):
     def __init__(
         self,
-        name:str,
+        name: str,
         contour: "Contour",
         img: img_type,
         img_weights: img_type | None = None,
@@ -456,49 +512,96 @@ class SolverGUI(Solver):
             # )
 
         # Adjust layout to make room for the button
+        self.fig.subplots_adjust(top=0.9)
         self.fig.subplots_adjust(bottom=0.2)
+
+        # Adding status text
+        self.ax_text = plt.axes((0.1, 0.95, 0.8, 0.05))
+        self.ax_text.set_axis_off()
+        self.status_string = "{n_finished:04d} strings drawn. {n_queue:04d} strings in the queue. Last score: {score:.4f}."
+        self.ax_text_text = self.ax_text.text(
+            0.95,
+            0.01,
+            self.status_string.format(n_finished=0, n_queue=0, score=0),
+            verticalalignment="bottom",
+            horizontalalignment="right",
+            transform=self.ax_text.transAxes,
+            fontsize=14,
+        )
 
         # Create a button below the subplots
         button_labels = ["1", "100", "500", "1000"]
         self._buttons = []
         for i, label in enumerate(button_labels):
             ax_button = plt.axes(
-                (0.1 + i * 0.2, 0.05, 0.15, 0.05)
+                (0.1 + i * 0.2, 0.1, 0.15, 0.05)
             )  # [left, bottom, width, height]
             button = widgets.Button(ax_button, label)
-            button.on_clicked(self._next_lines_GUI_call)
+            button.on_clicked(self._add_frames_to_queue)
             button.ax.set_label(label)
             self._buttons.append(button)
+        button_labels = ["stop"]
+        button_callbacks = [self._clear_queue]
+        for i, (label, callback) in enumerate(
+            zip(button_labels, button_callbacks)
+        ):
+            ax_button = plt.axes(
+                (0.1 + i * 0.2, 0.02, 0.15, 0.05)
+            )  # [left, bottom, width, height]
+            button = widgets.Button(ax_button, label)
+            button.on_clicked(callback)
+            button.ax.set_label(label)
+            self._buttons.append(button)
+        self._framequeue = 0
 
     def start_gui(self):
-        plt.show(block=True)
+        # plt.ion()
+        plt.show(block=False)
+        self._running = True
+        self._mainloop()
 
-    def solve_n_lines(self, n=1):
-        for i in range(n):
-            self.solve_next()
-            self._update_gui()
+    def _mainloop(self):
+        # I know that fig.number exists at runntime, the linter does not. Thus type: ignore
+        while self._running and plt.fignum_exists(self.fig.number):  # type: ignore
+            if self._framequeue > 0:
+                self.solve_next()
+                self._update_gui()
+                self._framequeue -= 1
+            self.fig.canvas.start_event_loop(0.001)  # this makes the mpl window responsive
 
-    def _next_lines_GUI_call(self, event):
-        n_calls = int(event.inaxes.get_label())
-        self.solve_n_lines(n_calls)
+    def _add_frames_to_queue(self, event):
+        n_frames = int(event.inaxes.get_label())
+        self._framequeue += n_frames
+
+    def _clear_queue(self, event):
+        self._framequeue = 0
+        self._update_gui()
 
     def _update_gui(self):
         self.mpl_img_canvas.set_data(self._img_canvas)
         self.mpl_img.set_data(self._img)
-        self.fig.canvas.draw()
+        self.ax_text_text.set_text(self.status_string.format(n_finished=self.string_count, n_queue=self._framequeue, score=self.curr_score))
+        # self.fig.canvas.draw()
+        self.fig.canvas.draw_idle()
         self.fig.canvas.flush_events()
 
 
 if __name__ == "__main__":
     circle = Circle((0, 0), 100)
-    img = cv2.imread(
-        r".\..\string_art\test_images\Snoopy_Peanuts.png"
-    )
+    img = cv2.imread(r".\..\string_art\test_images\Snoopy_Peanuts.png")
     img_weights = None
     # img_weights = cv2.imread(
     #     r".\..\string_art\test_images\11_mask.jpg"
     # )
-    solver = SolverGUI('test', circle, img, img_weights=img_weights, line_thickness=0.2, dpmm=5.0, n_points=500)
+    solver = SolverGUI(
+        "test",
+        circle,
+        img,
+        img_weights=img_weights,
+        line_thickness=0.2,
+        dpmm=5.0,
+        n_points=500,
+    )
     solver.start_gui()
     print(solver.s_connections)
     # solver.solve_next()
@@ -511,5 +614,3 @@ if __name__ == "__main__":
     gcode_stringer = GcodeStringer(solver, gsketch)
     gcode_stringer.process_Gcode()
     print(gsketch.get_GCode())
-
-
